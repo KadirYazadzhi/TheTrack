a89588201ad2ade797abd0b77303d4bfc2960dc1
kadir7563483 <kyuya_2020g@pgiblg.com>
2023-11-25 00:49:21 +0200

    new

---
+++ Script/Double-click.js
@@ -0,0 +1,146 @@
+// Получаваме референции към елементите
+const modal = document.getElementById('doubleClickModal-ZampRZ-42');
+const acceptButton = document.getElementById('acceptButton-ZampRZ-42');
+const cancelButton = document.getElementById('cancelButton-ZampRZ-42');
+const myElement = document.getElementById('Zamp-RZ42');
+
+// Функция за отваряне на модалното прозорче
+function openModalZampRZ42() {
+    modal.style.display = 'block';
+}
+
+// Функция за затваряне на модалното прозорче
+function closeModalZampRZ42() {
+    modal.style.display = 'none';
+}
+
+// Обработка на двойно кликване върху елемента
+myElement.addEventListener('dblclick', function() {
+    openModalZampRZ42();
+});
+
+// Обработка на клик върху бутона "Приемам"
+acceptButton.addEventListener('click', function() {
+    // Променете URL-а на страницата, към която искате да пренасочите потребителя
+    window.location.href = 'Zamp%20RZ42.html';
+    closeModalZampRZ42();
+});
+
+// Обработка на клик върху бутона "Отказвам"
+cancelButton.addEventListener('click', function() {
+    closeModalZampRZ42();
+});
+
+// Получаваме референции към елементите
+const modalMinus = document.getElementById('doubleClickModal-Minus273-Drip');
+const acceptButtonMinus = document.getElementById('acceptButton-Minus273-Drip');
+const cancelButtonMinus = document.getElementById('cancelButton-Minus273-Drip');
+const myElementMinus = document.getElementById('Minus273-Drip');
+
+// Функция за отваряне на модалното прозорче
+function openModalMinus273Drip() {
+    modalMinus.style.display = 'block';
+}
+
+// Функция за затваряне на модалното прозорче
+function closeModalMinus273Drip() {
+    modalMinus.style.display = 'none';
+}
+
+// Обработка на двойно кликване върху елемента
+myElementMinus.addEventListener('dblclick', function() {
+    openModalMinus273Drip();
+});
+
+// Обработка на клик върху бутона "Приемам"
+acceptButtonMinus.addEventListener('click', function() {
+    // Променете URL-а на страницата, към която искате да пренасочите потребителя
+    window.location.href = 'Minus273%20Drip.html';
+    closeModalMinus273Drip();
+});
+
+// Обработка на клик върху бутона "Отказвам"
+cancelButtonMinus.addEventListener('click', function() {
+    closeModalMinus273Drip();
+});
+
+
+
+
+
+
+// Получаваме референции към елементите
+const modalOMPOneArt = document.getElementById('doubleClickModal-OMP-One-Art');
+const acceptButtonOMPOneArt = document.getElementById('acceptButton-OMP-One-Art');
+const cancelButtonOMPOneArt = document.getElementById('cancelButton-OMP-One-Art');
+const myElementOMPOneArt = document.getElementById('OMP-One-Art');
+
+// Функция за отваряне на модалното прозорче
+function openModalOMPOneArt() {
+    modalOMPOneArt.style.display = 'block';
+}
+
+// Функция за затваряне на модалното прозорче
+function closeModalOMPOneArt() {
+    modalOMPOneArt.style.display = 'none';
+}
+
+// Обработка на двойно кликване върху елемента
+myElementOMPOneArt.addEventListener('dblclick', function() {
+    openModalOMPOneArt();
+});
+
+// Обработка на клик върху бутона "Приемам"
+acceptButtonOMPOneArt.addEventListener('click', function() {
+    // Променете URL-а на страницата, към която искате да пренасочите потребителя
+    window.location.href = 'OMP%20ONE%20ART.html';
+    closeModalOMPOneArt();
+});
+
+// Обработка на клик върху бутона "Отказвам"
+cancelButtonOMPOneArt.addEventListener('click', function() {
+    closeModalOMPOneArt();
+});
+
+
+
+
+
+
+// Получаваме референции към елементите
+const modalAlpinestarsKazeTech1 = document.getElementById('doubleClickModal-Alpinestars-Kaze-Tech-1');
+const acceptButtonAlpinestarsKazeTech1 = document.getElementById('acceptButton-Alpinestars-Kaze-Tech-1');
+const cancelButtonAlpinestarsKazeTech1 = document.getElementById('cancelButton-Alpinestars-Kaze-Tech-1');
+const myElementAlpinestarsKazeTech1 = document.getElementById('Alpinestars-Kaze-Tech-1');
+
+// Функция за отваряне на модалното прозорче
+function openModalAlpinestarsKazeTech1() {
+    modalAlpinestarsKazeTech1.style.display = 'block';
+}
+
+// Функция за затваряне на модалното прозорче
+function closeModalAlpinestarsKazeTech1() {
+    modalAlpinestarsKazeTech1.style.display = 'none';
+}
+
+// Обработка на двойно кликване върху елемента
+myElementAlpinestarsKazeTech1.addEventListener('dblclick', function() {
+    openModalAlpinestarsKazeTech1();
+});
+
+// Обработка на клик върху бутона "Приемам"
+acceptButtonAlpinestarsKazeTech1.addEventListener('click', function() {
+    // Променете URL-а на страницата, към която искате да пренасочите потребителя
+    window.location.href = 'Alpinestars%20Kaze%20Tech-1.html';
+    closeModalAlpinestarsKazeTech1();
+});
+
+// Обработка на клик върху бутона "Отказвам"
+cancelButtonAlpinestarsKazeTech1.addEventListener('click', function() {
+    closeModalAlpinestarsKazeTech1();
+});
+
+
+
+
+
---
+++ Script/Novini.js
@@ -0,0 +1,4 @@
+function toggleCardSize() {
+    var card = document.getElementById("card");
+    card.classList.toggle("expanded-card");
+}
