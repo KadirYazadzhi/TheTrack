1aed4ca9d72961e5d569d066a2d3fcf4fb8b074a
kadir7563483 <kyuya_2020g@pgiblg.com>
2023-11-26 18:22:52 +0200

    new

---
+++ Script/Price-Karting.js
@@ -0,0 +1,61 @@
+/*G0-Kart-1*/
+function changeTextKart15() {
+    document.getElementById("Kart-1").textContent = "10 лв.";
+}
+function changeTextKart110() {
+    document.getElementById("Kart-1").textContent = "17 лв.";
+}
+function changeTextKart115() {
+    document.getElementById("Kart-1").textContent = "26 лв.";
+}
+function changeTextKart120() {
+    document.getElementById("Kart-1").textContent = "30 лв.";
+}
+
+
+
+/*G0-Kart-2*/
+function changeTextKart25() {
+    document.getElementById("Kart-2").textContent = "10 лв.";
+}
+function changeTextKart210() {
+    document.getElementById("Kart-2").textContent = "17 лв.";
+}
+function changeTextKart215() {
+    document.getElementById("Kart-2").textContent = "26 лв.";
+}
+function changeTextKart220() {
+    document.getElementById("Kart-2").textContent = "31 лв.";
+}
+
+
+/*G0-Kart-3*/
+function changeTextKart35() {
+    document.getElementById("Kart-3").textContent = "8 лв.";
+}
+function changeTextKart310() {
+    document.getElementById("Kart-3").textContent = "14 лв.";
+}
+function changeTextKart315() {
+    document.getElementById("Kart-3").textContent = "23 лв.";
+}
+function changeTextKart320() {
+    document.getElementById("Kart-3").textContent = "30 лв.";
+}
+
+
+
+/*G0-Kart-4*/
+function changeTextKart45() {
+    document.getElementById("Kart-4").textContent = "15 лв.";
+}
+function changeTextKart410() {
+    document.getElementById("Kart-4").textContent = "26 лв.";
+}
+function changeTextKart415() {
+    document.getElementById("Kart-4").textContent = "40 лв.";
+}
+function changeTextKart420() {
+    document.getElementById("Kart-4").textContent = "- лв.";
+}
+
