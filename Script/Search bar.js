633555b1287c3daa6bbc7f4e8fe901267f91dc78
kadir7563483 <kyuya_2020g@pgiblg.com>
2023-11-28 22:58:55 +0200

    new

---
+++ Script/Search bar.js
@@ -0,0 +1,81 @@
+function showChildrenkaski(element) {
+    const children = element.querySelector('.kaski-children');
+    children.style.display = 'block';
+}
+
+function hideChildrenkaski(element) {
+    const children = element.querySelector('.kaski-children');
+    children.style.display = 'none';
+}
+function showChildrengashterizoni(element) {
+    const children = element.querySelector('.gashterizon-children');
+    children.style.display = 'block';
+}
+
+function hideChildrengashterizoni(element) {
+    const children = element.querySelector('.gashterizon-children');
+    children.style.display = 'none';
+}
+
+
+function showChildrenrukavici(element) {
+    const children = element.querySelector('.rukavici-children');
+    children.style.display = 'block';
+}
+
+function hideChildrenrukavici(element) {
+    const children = element.querySelector('.rukavici-children');
+    children.style.display = 'none';
+}
+
+
+function showChildrenobuvki(element) {
+    const children = element.querySelector('.obuvki-children');
+    children.style.display = 'block';
+}
+
+function hideChildrenobuvki(element) {
+    const children = element.querySelector('.obuvki-children');
+    children.style.display = 'none';
+}
+
+
+
+
+function showChildrenoborudvane(element) {
+    const children = element.querySelector('.oborudvane-children');
+    children.style.display = 'block';
+}
+
+function hideChildrenoborudvane(element) {
+    const children = element.querySelector('.oborudvane-children');
+    children.style.display = 'none';
+}
+
+
+
+
+function showChildrenaksesoari(element) {
+    const children = element.querySelector('.aksesoari-children');
+    children.style.display = 'block';
+}
+
+function hideChildrenaksesoari(element) {
+    const children = element.querySelector('.aksesoari-children');
+    children.style.display = 'none';
+}
+
+
+
+function showChildrenbox(element) {
+    const children = element.querySelector('.box-children');
+    children.style.display = 'block';
+}
+
+function hideChildrenbox(element) {
+    const children = element.querySelector('.box-children');
+    children.style.display = 'none';
+}
+
+
+
