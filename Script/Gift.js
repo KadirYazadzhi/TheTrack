65a27610cd466717389a8ac6f9c526db6cb4d226
kadir7563483 <kyuya_2020g@pgiblg.com>
2023-11-29 13:10:33 +0200

    new

--- Script/Gift.js
+++ Script/Gift.js
@@ -4,8 +4,25 @@ const master = document.getElementById("master");
 const basic = document.getElementById("basic");
 
 checkbox.addEventListener("click", () => {
-    basic.textContent = basic.textContent === "500 лв." ? "50 лв." : "500 лв.";
+    basic.textContent = basic.textContent === "900 лв." ? "100 лв." : "900 лв.";
     professional.textContent =
-        professional.textContent === "700 лв." ? "70 лв." : "700 лв.";
-    master.textContent = master.textContent === "1400 лв." ? "140 лв." : "1400 лв.";
+        professional.textContent === "1500 лв." ? "170 лв." : "1500 лв.";
+    master.textContent = master.textContent === "2400 лв." ? "300 лв." : "2400 лв.";
 });
+
+
+
+
+
+
+// Отваря модалния прозорец
+function openModal() {
+    var modal = document.getElementById('myModal');
+    modal.style.display = 'block';
+}
+
+// Затваря модалния прозорец
+function closeModal() {
+    var modal = document.getElementById('myModal');
+    modal.style.display = 'none';
+}
