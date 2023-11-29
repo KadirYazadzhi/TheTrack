2828cd8b4c05fe2c59e370272becec94442c5dc7
kadir7563483 <kyuya_2020g@pgiblg.com>
2023-11-29 14:17:02 +0200

    new

--- Script/Gift.js
+++ Script/Gift.js
@@ -6,8 +6,33 @@ const basic = document.getElementById("basic");
 checkbox.addEventListener("click", () => {
     basic.textContent = basic.textContent === "900 лв." ? "100 лв." : "900 лв.";
     professional.textContent =
-        professional.textContent === "1500 лв." ? "170 лв." : "1500 лв.";
-    master.textContent = master.textContent === "2400 лв." ? "300 лв." : "2400 лв.";
+        professional.textContent === "2 400 лв." ? "300 лв." : "2 400 лв.";
+    master.textContent = master.textContent === "1 500 лв." ? "170 лв." : "1 500 лв.";
+});
+
+
+
+function closeModalVoucher() {
+    var modal = document.getElementById('popupBox');
+    modal.style.display = 'none';
+}
+
+
+
+
+
+
+const popup = document.querySelector(".popupBox");
+const close = document.querySelector(".close");
+
+window.onload = function () {
+    setTimeout(() => {
+        popup.style.display = "block";
+    }, 3000);
+};
+
+close.addEventListener("click", () => {
+    popup.style.display = "none";
 });
 
 
@@ -25,4 +50,20 @@ function openModal() {
 function closeModal() {
     var modal = document.getElementById('myModal');
     modal.style.display = 'none';
+}
+
+
+
+
+function openModalVoucherNew() {
+    var modal = document.getElementById('myModalVoucher');
+    modal.style.display = 'block';
+}
+
+// Затваря модалния прозорец
+function closeModalVoucherNew() {
+    var modal = document.getElementById('myModalVoucher');
+    modal.style.display = 'none';
+    var Newmodal = document.getElementById('popupBox');
+    Newmodal.style.display = 'none';
 }
