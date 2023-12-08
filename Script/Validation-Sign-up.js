6f49bb8f4d11b290fffa98f215530302610c4448
kadir7563483 <148765216+TheTrackOne@users.noreply.github.com>
2023-12-08 22:57:21 +0200

    new

--- Script/Validation-Sign-up.js
+++ Script/Validation-Sign-up.js
@@ -1,60 +1,65 @@
-
-
-
-function validateField(fieldName) {
-    const field = document.getElementById(fieldName);
-    const errorElement = document.getElementById(`${fieldName}Error`);
-    const isValid = field.value.trim() !== '';
-
-    errorElement.innerText = isValid ? '' : `Please enter ${fieldName === 'password' ? 'the' : 'your'} ${fieldName}.`;
-    field.classList.toggle('error', !isValid);
-    field.classList.toggle('valid', isValid);
-}
-
-function validateForm() {
-    const name = document.getElementById('name');
-    const email = document.getElementById('email');
-    const password = document.getElementById('password');
-    const formbtn = document.getElementById('submitprofile');
-
-
-    if (formbtn) {
-        formbtn.addEventListener('click', function () {
-            name.style.backgroundColor = name.value.trim() === '' ? '#ffeded' : 'transparent';
-            email.style.backgroundColor = email.value.trim() === '' ? '#ffeded' : 'transparent';
-            password.style.backgroundColor = password.value.trim() === '' ? '#ffeded' : 'transparent';
-        });
-    }
-
-    validateField('name');
-    validateField('email');
-    validateField('password');
-
-
-}
 document.addEventListener('DOMContentLoaded', function () {
     const userParagraph = document.getElementById('user');
-    const loginModal = document.getElementById('login');
     const formbtn = document.getElementById('submitprofile');
+    const login = document.getElementById('smalllogin');
+    const loginModal = document.getElementById('biglogin');
+
+    function validateField(fieldName) {
+        const field = document.getElementById(fieldName);
+        const errorElement = document.getElementById(`${fieldName}Error`);
+        const isValid = field.value.trim() !== '';
+
+        errorElement.innerText = isValid ? '' : `Please enter ${fieldName === 'password' ? 'the' : 'your'} ${fieldName}.`;
+        field.classList.toggle('error', !isValid);
+        field.classList.toggle('valid', isValid);
+
+        return isValid;
+    }
+
+    function validateForm() {
+        const name = document.getElementById('name');
+        const email = document.getElementById('email');
+        const password = document.getElementById('password');
+
+        const isNameValid = validateField('name');
+        const isEmailValid = validateField('email');
+        const isPasswordValid = validateField('password');
+
+        return isNameValid && isEmailValid && isPasswordValid;
+    }
+
+    function handleInputChange(event) {
+        const field = event.target;
+        field.style.backgroundColor = field.value.trim() === '' ? '#ffeded' : 'transparent';
+        field.classList.add('valid');
+
+        const errorElement = document.getElementById(`${field.id}Error`);
+        if (errorElement && field.value.trim() !== '') {
+            errorElement.innerText = '';
+        }
+    }
+
+    const inputFields = document.querySelectorAll('.input-profile');
+    inputFields.forEach(function (inputField) {
+        inputField.addEventListener('input', handleInputChange);
+    });
 
     if (formbtn) {
         formbtn.addEventListener('click', function (event) {
             event.preventDefault();
 
-            // Промяна на стойността в параграфа и запис в Local Storage
-            const name = document.getElementById('name');
-            if (name.value.trim() !== '') {
-                userParagraph.innerText = ` ${name.value.trim()}`;
-                localStorage.setItem('userName', name.value.trim());
-            }
+            const isFormValid = validateForm();
 
-            // Затваряне на модалната форма
-            loginModal.classList.remove('show');
-            loginModal.style.display = 'none';
-            document.body.classList.remove('modal-open');
-            const modalBackdrop = document.getElementsByClassName('modal-backdrop')[0];
-            if (modalBackdrop) {
-                modalBackdrop.parentNode.removeChild(modalBackdrop);
+            if (isFormValid) {
+                const name = document.getElementById('name');
+
+                if (name.value.trim() !== '') {
+                    userParagraph.innerText = ` ${name.value.trim()}`;
+                    localStorage.setItem('userName', name.value.trim());
+                }
+
+                loginModal.style.display = 'none';
+                login.style.display = 'none';
             }
         });
     }
@@ -63,7 +68,26 @@ document.addEventListener('DOMContentLoaded', function () {
     const storedUserName = localStorage.getItem('userName');
     if (storedUserName) {
         userParagraph.innerText = ` ${storedUserName}`;
-        loginModal.style.display = 'none'
     }
 
+    // Проверка при отваряне на модалната форма
+    document.addEventListener('click', function (event) {
+        if (event.target === userParagraph && userParagraph.innerText !== 'User') {
+            loginModal.style.display = 'none';
+            login.style.display = 'none';
+        }
+    });
+
+    // Показване на модалната форма
+    userParagraph.addEventListener('click', function () {
+        if (userParagraph.innerText === 'User') {
+            loginModal.style.display = 'block';
+            login.style.display = 'block';
+        }
+    });
 });
+
+function closeLoginForm() {
+    const loginModal = document.getElementById('biglogin');
+    loginModal.style.display = 'none';
+}
